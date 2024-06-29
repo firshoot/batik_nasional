7e319068e0fd01210d277fdbfe6dd11869c59a15
MegaOfCyber <126873186+MegaOfCyber@users.noreply.github.com>
2024-06-30 05:04:53 +0700

    membenarkan detail screen
    sudah berhasil membenarkan error yang terus menerus membuat kodingan kacaw(gambar tidak terload)

--- batik_nasional/lib/detail_screen.dart
+++ batik_nasional/lib/detail_screen.dart
@@ -8,6 +8,8 @@ class Batik {
   final String built;
   final String type;
   final String description;
+  final String imageAsset;
+  final List<String> imageUrls;
 
   Batik({
     required this.name,
@@ -15,15 +17,15 @@ class Batik {
     required this.built,
     required this.type,
     required this.description,
+    required this.imageAsset,
+    required this.imageUrls,
   });
 }
 
 class DetailScreen extends StatefulWidget {
   final Batik batik;
-  final void Function(Set<String>)? updateFavorites;
 
-  const DetailScreen({Key? key, required this.batik, this.updateFavorites})
-      : super(key: key);
+  const DetailScreen({Key? key, required this.batik}) : super(key: key);
 
   @override
   _DetailScreenState createState() => _DetailScreenState();
@@ -31,10 +33,14 @@ class DetailScreen extends StatefulWidget {
 
 class _DetailScreenState extends State<DetailScreen> {
   bool isSignedIn = false;
-  bool _isFavorite = false;
-  late Set<String> favoritebatiks = <String>{};
 
-  void _checkSignInStatus() async {
+  @override
+  void initState() {
+    super.initState();
+    _checkSignInStatus();
+  }
+
+  Future<void> _checkSignInStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool signedIn = prefs.getBool('isSignedIn') ?? false;
 
@@ -44,87 +50,49 @@ class _DetailScreenState extends State<DetailScreen> {
   }
 
   @override
-  void initState() {
-    super.initState();
-    _checkSignInStatus();
-    _loadFavouriteStatus();
-  }
-
-  Future<void> _loadFavouriteStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool favorite = prefs.getBool('favorite_${widget.batik.name}') ?? false;
-
-    setState(() {
-      _isFavorite = favorite;
-      favoritebatiks =
-          prefs.getStringList('favoritebatiks')?.toSet() ?? <String>{};
-    });
-  }
-
-  Future<void> _toggleFavorite() async {
-    bool isFavorite = !_isFavorite;
-    await _saveFavoriteStatus(isFavorite);
-    setState(() {
-      _isFavorite = isFavorite;
-    });
-  }
-
-  Future<void> _saveFavoriteStatus(bool isFavorite) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String batikName = widget.batik.name;
-
-    if (isFavorite) {
-      favoritebatiks.add(batikName);
-    } else {
-      favoritebatiks.remove(batikName);
-    }
-
-    await prefs.setStringList('favoritebatiks', favoritebatiks.toList());
-    await prefs.setBool('favorite_$batikName', isFavorite);
-
-    // Beritahu widget induk (HomeScreen) bahwa ada perubahan pada daftar favorit
-    if (widget.updateFavorites != null) {
-      widget.updateFavorites!(favoritebatiks);
-    }
-  }
-
-  Future<void> _removeFromFavorites() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String batikName = widget.batik.name;
-
-    if (favoritebatiks.contains(batikName)) {
-      favoritebatiks.remove(batikName);
-      await prefs.setStringList('favoritebatiks', favoritebatiks.toList());
-      await prefs.remove('favorite_$batikName');
-
-      // Beritahu widget induk (HomeScreen) bahwa ada perubahan pada daftar favorit
-      if (widget.updateFavorites != null) {
-        widget.updateFavorites!(favoritebatiks);
-      }
-    }
-  }
-
-  @override
   Widget build(BuildContext context) {
     return Scaffold(
       body: SingleChildScrollView(
         child: Column(
-          crossAxisAlignment: CrossAxisAlignment.start,
           children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
-              child: Container(
-                decoration: BoxDecoration(
-                  color: Colors.blue.shade100.withOpacity(0.5),
-                  shape: BoxShape.circle,
+            Stack(
+              children: [
+                Padding(
+                  padding: const EdgeInsets.symmetric(horizontal: 8),
+                  child: ClipRRect(
+                    borderRadius: BorderRadius.circular(20),
+                    child: widget.batik.imageAsset.isNotEmpty
+                        ? Image.network(
+                            widget.batik.imageAsset,
+                            width: double.infinity,
+                            height: 300,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            height: 300,
+                            width: double.infinity,
+                            color: Colors.grey,
+                            child: Center(child: Text('No Image')),
+                          ),
+                  ),
                 ),
-                child: IconButton(
-                  onPressed: () {
-                    Navigator.pop(context);
-                  },
-                  icon: const Icon(Icons.arrow_back),
+                Padding(
+                  padding:
+                      const EdgeInsets.symmetric(horizontal: 16, vertical: 32),
+                  child: Container(
+                    decoration: BoxDecoration(
+                      color: Colors.blue.shade100.withOpacity(0.5),
+                      shape: BoxShape.circle,
+                    ),
+                    child: IconButton(
+                      onPressed: () {
+                        Navigator.pop(context);
+                      },
+                      icon: const Icon(Icons.arrow_back),
+                    ),
+                  ),
                 ),
-              ),
+              ],
             ),
             Padding(
               padding: const EdgeInsets.symmetric(horizontal: 16),
@@ -139,52 +107,22 @@ class _DetailScreenState extends State<DetailScreen> {
                       fontWeight: FontWeight.bold,
                     ),
                   ),
-                  const SizedBox(height: 4),
+                  const SizedBox(height: 8),
                   Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
                     children: [
-                      Row(
-                        children: [
-                          const Icon(
-                            Icons.place,
-                            color: Colors.red,
-                          ),
-                          const SizedBox(width: 8),
-                          const SizedBox(
-                            width: 70,
-                            child: Text(
-                              'Lokasi',
-                              style: TextStyle(fontWeight: FontWeight.bold),
-                            ),
-                          ),
-                          Text(' : ${widget.batik.location}')
-                        ],
+                      const Icon(
+                        Icons.place,
+                        color: Colors.red,
                       ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              _toggleFavorite();
-                            },
-                            icon: Icon(
-                              isSignedIn && _isFavorite
-                                  ? Icons.favorite
-                                  : Icons.favorite_border,
-                            ),
-                            color:
-                                isSignedIn && _isFavorite ? Colors.red : null,
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              _removeFromFavorites();
-                            },
-                            icon: Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
+                      const SizedBox(width: 8),
+                      const SizedBox(
+                        width: 70,
+                        child: Text(
+                          'Lokasi',
+                          style: TextStyle(fontWeight: FontWeight.bold),
+                        ),
                       ),
+                      Text(' : ${widget.batik.location}')
                     ],
                   ),
                   Row(
@@ -230,6 +168,50 @@ class _DetailScreenState extends State<DetailScreen> {
                   ),
                   const SizedBox(height: 8),
                   Text(widget.batik.description),
+                ],
+              ),
+            ),
+            Padding(
+              padding: const EdgeInsets.all(15),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Divider(color: Colors.blue.shade100),
+                  const Text(
+                    'Galeri',
+                    style: TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+                  ),
+                  const SizedBox(height: 8),
+                  SizedBox(
+                    height: 100,
+                    child: ListView.builder(
+                      scrollDirection: Axis.horizontal,
+                      itemCount: widget.batik.imageUrls.length,
+                      itemBuilder: (context, index) {
+                        return Padding(
+                          padding: const EdgeInsets.only(right: 8),
+                          child: GestureDetector(
+                            onTap: () {
+                              // Aksi saat gambar di galeri ditekan
+                            },
+                            child: Container(
+                              decoration: BoxDecoration(),
+                              child: ClipRRect(
+                                borderRadius: BorderRadius.circular(10),
+                                child: CachedNetworkImage(
+                                  imageUrl: widget.batik.imageUrls[index],
+                                  placeholder: (context, url) =>
+                                      CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      Icon(Icons.error),
+                                ),
+                              ),
+                            ),
+                          ),
+                        );
+                      },
+                    ),
+                  ),
                 ],
               ),
             ),
--- batik_nasional/lib/home_screen.dart
+++ batik_nasional/lib/home_screen.dart
@@ -1,14 +1,14 @@
+import 'package:cached_network_image/cached_network_image.dart';
 import 'package:cloud_firestore/cloud_firestore.dart'; // Import Firestore
 import 'package:batik_nasional/add_post_screen.dart'; // Import AddPostScreen
 import 'package:batik_nasional/sign_in_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_auth/firebase_auth.dart' as auth;
 import 'package:shared_preferences/shared_preferences.dart';
-import 'package:cached_network_image/cached_network_image.dart';
 
 class Batik {
   final String name;
-  final String imageAsset;
+  final String imageAsset; // Di sini kita akan simpan URL gambar.
   final String location;
   final String built;
   final String type;
@@ -17,7 +17,7 @@ class Batik {
 
   Batik({
     required this.name,
-    required this.imageAsset,
+    required this.imageAsset, // URL gambar akan digunakan di sini.
     required this.location,
     required this.built,
     required this.type,
@@ -104,11 +104,8 @@ class HomeScreen extends StatelessWidget {
                               description: document['description'],
                               imageUrls: [
                                 document['imageUrl']
-                              ], // Adjust this as needed
+                              ], // URL gambar digunakan di sini.
                             ),
-                            updateFavorites: (favorites) {
-                              // Implement your update logic here
-                            },
                           ),
                         ));
                       },
@@ -144,10 +141,8 @@ class HomeScreen extends StatelessWidget {
 
 class DetailScreen extends StatefulWidget {
   final Batik batik;
-  final void Function(Set<String>)? updateFavorites;
 
-  const DetailScreen({Key? key, required this.batik, this.updateFavorites})
-      : super(key: key);
+  const DetailScreen({Key? key, required this.batik}) : super(key: key);
 
   @override
   _DetailScreenState createState() => _DetailScreenState();
@@ -155,77 +150,20 @@ class DetailScreen extends StatefulWidget {
 
 class _DetailScreenState extends State<DetailScreen> {
   bool isSignedIn = false;
-  bool _isFavorite = false;
-  late Set<String> favoritebatiks = <String>{};
 
-  void _checkSignInStatus() async {
+  @override
+  void initState() {
+    super.initState();
+    _checkSignInStatus();
+  }
+
+  Future<void> _checkSignInStatus() async {
     SharedPreferences prefs = await SharedPreferences.getInstance();
     bool signedIn = prefs.getBool('isSignedIn') ?? false;
 
     setState(() {
       isSignedIn = signedIn;
     });
-  }
-
-  @override
-  void initState() {
-    super.initState();
-    _checkSignInStatus();
-    _loadFavouriteStatus();
-  }
-
-  Future<void> _loadFavouriteStatus() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    bool favorite = prefs.getBool('favorite_${widget.batik.name}') ?? false;
-
-    setState(() {
-      _isFavorite = favorite;
-      favoritebatiks =
-          prefs.getStringList('favoritebatiks')?.toSet() ?? <String>{};
-    });
-  }
-
-  Future<void> _toggleFavorite() async {
-    bool isFavorite = !_isFavorite;
-    await _saveFavoriteStatus(isFavorite);
-    setState(() {
-      _isFavorite = isFavorite;
-    });
-  }
-
-  Future<void> _saveFavoriteStatus(bool isFavorite) async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String batikName = widget.batik.name;
-
-    if (isFavorite) {
-      favoritebatiks.add(batikName);
-    } else {
-      favoritebatiks.remove(batikName);
-    }
-
-    await prefs.setStringList('favoritebatiks', favoritebatiks.toList());
-    await prefs.setBool('favorite_$batikName', isFavorite);
-
-    // Beritahu widget induk (HomeScreen) bahwa ada perubahan pada daftar favorit
-    if (widget.updateFavorites != null) {
-      widget.updateFavorites!(favoritebatiks);
-    }
-  }
-
-  Future<void> _removeFromFavorites() async {
-    SharedPreferences prefs = await SharedPreferences.getInstance();
-    final String batikName = widget.batik.name;
-
-    if (favoritebatiks.contains(batikName)) {
-      favoritebatiks.remove(batikName);
-      await prefs.setStringList('favoritebatiks', favoritebatiks.toList());
-      await prefs.remove('favorite_$batikName');
-
-      // Beritahu widget induk (HomeScreen) bahwa ada perubahan pada daftar favorit
-      if (widget.updateFavorites != null) {
-        widget.updateFavorites!(favoritebatiks);
-      }
-    }
   }
 
   @override
@@ -240,12 +178,19 @@ class _DetailScreenState extends State<DetailScreen> {
                   padding: const EdgeInsets.symmetric(horizontal: 8),
                   child: ClipRRect(
                     borderRadius: BorderRadius.circular(20),
-                    child: Image.asset(
-                      widget.batik.imageAsset,
-                      width: double.infinity,
-                      height: 300,
-                      fit: BoxFit.cover,
-                    ),
+                    child: widget.batik.imageAsset.isNotEmpty
+                        ? Image.network(
+                            widget.batik.imageAsset,
+                            width: double.infinity,
+                            height: 300,
+                            fit: BoxFit.cover,
+                          )
+                        : Container(
+                            height: 300,
+                            width: double.infinity,
+                            color: Colors.grey,
+                            child: Center(child: Text('No Image')),
+                          ),
                   ),
                 ),
                 Padding(
@@ -272,44 +217,14 @@ class _DetailScreenState extends State<DetailScreen> {
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: [
                   const SizedBox(height: 8),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    children: [
-                      Text(
-                        widget.batik.name,
-                        style: const TextStyle(
-                          fontSize: 20,
-                          fontWeight: FontWeight.bold,
-                        ),
-                      ),
-                      Row(
-                        children: [
-                          IconButton(
-                            onPressed: () {
-                              _toggleFavorite();
-                            },
-                            icon: Icon(
-                              isSignedIn && _isFavorite
-                                  ? Icons.favorite
-                                  : Icons.favorite_border,
-                            ),
-                            color:
-                                isSignedIn && _isFavorite ? Colors.red : null,
-                          ),
-                          IconButton(
-                            onPressed: () {
-                              _removeFromFavorites();
-                            },
-                            icon: Icon(
-                              Icons.delete,
-                              color: Colors.red,
-                            ),
-                          ),
-                        ],
-                      ),
-                    ],
+                  Text(
+                    widget.batik.name,
+                    style: const TextStyle(
+                      fontSize: 20,
+                      fontWeight: FontWeight.bold,
+                    ),
                   ),
-                  const SizedBox(height: 4),
+                  const SizedBox(height: 8),
                   Row(
                     children: [
                       const Icon(
@@ -393,13 +308,19 @@ class _DetailScreenState extends State<DetailScreen> {
                         return Padding(
                           padding: const EdgeInsets.only(right: 8),
                           child: GestureDetector(
-                            onTap: () {},
+                            onTap: () {
+                              // Aksi saat gambar di galeri ditekan
+                            },
                             child: Container(
                               decoration: BoxDecoration(),
                               child: ClipRRect(
                                 borderRadius: BorderRadius.circular(10),
                                 child: CachedNetworkImage(
                                   imageUrl: widget.batik.imageUrls[index],
+                                  placeholder: (context, url) =>
+                                      CircularProgressIndicator(),
+                                  errorWidget: (context, url, error) =>
+                                      Icon(Icons.error),
                                 ),
                               ),
                             ),
